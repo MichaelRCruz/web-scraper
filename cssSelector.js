e82c99f4af604e3104ef0fe3fedef89105e483a4
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-19 15:17:38 -0700

    modifies function

--- cssSelector.js
+++ cssSelector.js
@@ -2,17 +2,17 @@ var casper = require('casper').create();
 
 var websites = ['http://michaelcruz.io', 'https://www.google.com/', 'https://github.com/'];
 
-function scraper() {
-  for (var i = 0; i < websites.length; i++) {
-    casper.start(i, function() {
+function scraper(array) {
+  for (var i = 0; i < array.length; i++) {
+    casper.start(websites[i], function() {
         if (this.exists('div.splash')) {
-            this.echo('the heading exists');
+          this.echo('the heading exists');
         } else {
-          this.echo('nada')
+            this.echo('nada')
         }
     });
     casper.run();
   }
 }
 
-scraper();
+scraper(websites);
