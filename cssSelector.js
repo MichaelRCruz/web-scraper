6570485edaef7d3731813d3cb7a648b4c997605e
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-19 16:16:38 -0700

    adds casper waitFor

--- cssSelector.js
+++ cssSelector.js
@@ -2,17 +2,35 @@ var casper = require('casper').create();
 
 var websites = ['http://michaelcruz.io', 'https://www.google.com/', 'https://github.com/'];
 
-function scraper(array) {
-  for (var i = 0; i < array.length; i++) {
-    casper.start(websites[i], function() {
-        if (this.exists('div.splash')) {
-          this.echo('the heading exists');
-        } else {
-            this.echo('nada')
-        }
-    });
-    casper.run();
-  }
-}
+// casper.start(websites[0], function() {
+//     if (this.exists('div.splash')) {
+//       this.echo('the heading exists');
+//     } else {
+//         this.echo('nada')
+//     }
+// });
+// casper.run();
+//
+// function scrape() {
+//
+// }
 
-scraper(websites);
+function scrape(websites) {
+  for (var i = 0; i < websites.length; i++) {
+    casper.waitFor(function check(websites) {
+      return casper.start(websites[i], function() {
+      });
+      casper.run();
+    }, function then() {    // step to execute when check() is ok
+      if (this.exists('div.splash')) {
+        this.echo('the div exists');
+      } else {
+          this.echo('nada')
+      }
+    }, function timeout() { // step to execute if check has failed
+        this.echo("I can't haz my info.").exit();
+    });
+  }
+};
+
+scrape(websites);
