a3c41edac64519d77e881e0149998c612c5922fe
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-19 19:50:15 -0700

    refactors to recurrsive function

--- cssSelector.js
+++ cssSelector.js
@@ -1,35 +1,36 @@
 var casper = require('casper').create();
 
 var websites = ['http://michaelcruz.io', 'https://www.google.com/', 'https://github.com/'];
-
-// casper.start(websites[0], function() {
-//     if (this.exists('div.splash')) {
-//       this.echo('the heading exists');
-//     } else {
-//         this.echo('nada')
-//     }
-// });
-// casper.run();
+var repairCount = 0;
 
 function scrape() {
-  for (var i = 0; i < websites.length; i++) {
-    casper.start(websites[i], function() {
-      casper.waitFor(function check() {
-          return this.evaluate(function() {
-              return document.querySelectorAll('html');
-          });
-      }, function then() {    // step to execute when check() is ok
-            if (this.exists('div.splash')) {
-              this.echo('the heading exists');
-            } else {
-                this.echo('nada')
-            }
-      }, function timeout() { // step to execute if check has failed
-          this.echo("I can't haz my info.").exit();
-      });
+  casper.start(websites[0], function() {
+    casper.waitFor(function check() {
+        return this.evaluate(function() {
+            return document.querySelectorAll('html');
+        });
+    }, function then() {    // step to execute when check() is ok
+          if (this.exists('div.splash')) {
+            this.echo('the element exists');
+          } else {
+              repairCount++;
+              this.echo('no element: ' + websites[0]);
+          }
+          beginScrape();
+    }, function timeout() { // step to execute if check has failed
+        this.echo("I can't haz my info.").exit();
     });
-    casper.run();  
-  }
-}
+  });
+  casper.run();
+  websites.shift();
+};
 
-scrape();
+
+function beginScrape() {
+  if (websites.length == 0) {
+    console.log(repairCount + ' websites in need of repair.');
+  }
+  scrape();
+};
+
+beginScrape();
