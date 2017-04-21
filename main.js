2fb1662f8128316f29bbd99c53487a4274ceadf8
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-20 21:51:25 -0700

    adds node

---
+++ main.js
@@ -0,0 +1,11 @@
+var redis = require("redis"),
+    client = redis.createClient();
+
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
