e0c3f10ebe3db8927ea072dedad80756fc9fe598
Michael Cruz <michael.cruz@uwalumni.com>
2017-04-25 22:30:23 -0700

    adds phantomjs for dynamically rendered content

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-start description
+## Web Scraper
+
+This is a web-scraping application designed to detect the existence of specified html elements. As of now, there are very obvious limitations to this application. The first being that I don't know of all the possible edge cases in which the HTML elements are rendered. Second, dynamically rendered content is not caught by the scraper. This was initially solved by using a virtual browser, but that came by means of Casper.js.
+
+#### Getting Started
--- script.py
+++ script.py
@@ -2,6 +2,12 @@ from bs4 import BeautifulSoup
 import redis
 import requests
 import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.PhantomJS()
+driver.set_window_size(1024, 768)
 
 r = redis.Redis()
 
@@ -51,6 +57,65 @@ while True:
         if 'chownow.com/static/js/iframe.js' in script.text:
             status[url] = 'SUCCESS'
             break
+
+    if status[url] == 'SUCCESS':
+        continue
+    print("falling back to phantomjs for ", url)
+
+    while len(driver.window_handles) > 1:
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+
+    driver.get(url)
+
+    sbtn = None
+    try:
+        sbtn = driver.find_element_by_partial_link_text('Order')
+        print("found Order button")
+    except:
+        print("no Order button")
+    if not sbtn:
+        try:
+            sbtn = driver.find_element_by_partial_link_text('ORDER')
+            print("found ORDER button")
+        except:
+            print("no ORDER button")
+        if not sbtn:
+            try:
+                sbtn = driver.find_element_by_xpath("//a[contains(@href, 'order')]")
+                print("found order button by URL")
+            except:
+                print("no order button by URL")
+
+    if not sbtn:
+        print("no button found")
+        continue
+
+    print("clicking button")
+    sbtn.click()
+    time.sleep(2)
+
+    if 'chownow' in driver.current_url:
+        print("page navigated to chownow")
+        status[url] = 'SUCCESS'
+        continue
+
+    elif len(driver.window_handles) > 1:
+        print("looks like a popup")
+        driver.switch_to.window(driver.window_handles[-1])
+        if 'chownow' in driver.current_url:
+            print("chownow opened in a new window")
+            status[url] = 'SUCCESS'
+            continue
+
+    else:
+        try:
+            iframe = driver.find_element_by_xpath("//iframe[contains(@src, 'chownow')]")
+            print("found chownow iframe")
+            status[url] = 'SUCCESS'
+            continue
+        except:
+            print("no chownow iframe found")
 
 print 'statuses:', status
 
